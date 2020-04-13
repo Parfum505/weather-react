63856cf3e29423e1aa9817d36710cd2a9a508d05
Pawel Kryvytskyi <pawel.kryvytskyi@traveltech.pl>
2020-04-13 17:44:55 +0200

    start weather-react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form";
+import Results from "./components/Results";
 
 function App() {
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?cnt=3&units=metric&APPID=0867a13b59c398d1edd05d49f440e4f0&q=';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
+      <Results />
     </div>
   );
 }
