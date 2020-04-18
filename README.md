e9210be79d7875eb29fd14847cf1f389b5d33ff3
Pawel Kryvytskyi <pawel.kryvytskyi@traveltech.pl>
2020-04-18 19:00:33 +0200

    add new gh-page

---
+++ README.md
@@ -0,0 +1,7 @@
+## Weather App
+
+![Weather App](public/img/weather.png "Weather App")
+
+#### Link to the demo [ Weather App](https://parfum505.github.io/weather-react/ "Weather App")
+
+Weather App uses current & forecast weather data from [Weather API](https://openweathermap.org/api "Weather API")
---
+++ precache-manifest.4899c22166ecf563d005d0ef57c5cfca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff70455b1da2ceac129afe753ee094e",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "bd88e0f1b1cd2bc8088b",
+    "url": "/weather-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d1acd442494a5acd5ede",
+    "url": "/weather-react/static/css/main.dd498a71.chunk.css"
+  },
+  {
+    "revision": "bd88e0f1b1cd2bc8088b",
+    "url": "/weather-react/static/js/2.a41308bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-react/static/js/2.a41308bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1acd442494a5acd5ede",
+    "url": "/weather-react/static/js/main.d71ada81.chunk.js"
+  },
+  {
+    "revision": "bfd21c9bf32e0d473fe6",
+    "url": "/weather-react/static/js/runtime-main.af9d2256.js"
+  }
+]);
